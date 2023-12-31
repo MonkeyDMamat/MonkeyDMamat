33fa803d48b5d395b0b91a39d58414538c97908a
MonkeyDMamat <150282951+MonkeyDMamat@users.noreply.github.com>
2023-12-31 19:47:16 +0700

    Add files via upload

---
+++ sikasik23/pages/3_📞_Contact.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+def main():
+    st.set_page_config(page_icon="🌊")
+    st.sidebar.success("Select Page Above.")
+    st.title("Kontak kami")
+
+
+if __name__ == '__main__':
+    main()
